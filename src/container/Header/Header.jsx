1e514a7dccd9fad6e8defc15944488ad5de28827
Christian Sifuentes <me@radixpoint.dev>
2022-06-16 01:07:01 -0400

    Finished draft of Header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,13 +18,13 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id='home' className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className='app__header-info'
       >
-        <div className='app_header-badge'>
+        <div className='app__header-badge'>
           <div className='badge-cmp app__flex'>
             <span>👋</span>
             <div style={{ marginLeft: '20' }}>
