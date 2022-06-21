5fc16054994a9d19f0cc1b3909bccb3ace3cd6cc
Christian Sifuentes <me@radixpoint.dev>
2022-06-20 23:52:00 -0400

    Adding some small fixes

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => {
     return (
         <div className='app__social'>
             <div>
-                <BsTwitter />
-            </div>
-            <div>
-                <FaFacebookF />
+                <FaLinkedinIn />
             </div>
             <div>
                 <BsInstagram />
+            </div>
+            <div>
+                <FaGithub />
             </div>
         </div>
     )
