118fee5dcb1c8f2a7012303d2a886988b795a626
Christian Sifuentes <me@radixpoint.dev>
2022-06-20 20:03:54 -0400

    fixed slow page with multiple requests and adding Motion transition as High Order Component

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './About.scss';
 import { urlFor, client } from '../../client';
@@ -64,4 +64,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about')
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  "app__whitebg"
+);
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 });
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
@@ -21,16 +21,29 @@ const Work = () => {
         setWorks(data);
         setFilterWork(data);
       })
-  })
+  }, []);
 
-  const handleWorkFilter = (item) => { }
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  }
 
   return (
     <>
       <h2 className='head-text'>My Creative <span>Portfolio </span>section</h2>
 
       <div className='app__work-filter'>
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'Mobile App', 'Full-Stack', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
@@ -55,7 +68,7 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className='app__work-hover app__flex'
               >
-                <a href={work.projectlink} target=" blank" rel='noreferrer'>
+                <a href={work.projectLink} target=" blank" rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -65,7 +78,25 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
+                <a href={work.codeLink} target=" blank" rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
               </motion.div>
+            </div>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
             </div>
           </div>
         ))}
@@ -74,4 +105,8 @@ const Work = () => {
   )
 }
 
-export default Work;
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  "app__primarybg"
+);
